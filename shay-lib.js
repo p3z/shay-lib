6bd6df42647d4466308a0ef861171c55971cbda8
p3z linux pc <chenille.pottle@yahoo.co.uk>
2021-12-09 08:48:12 +0000

    Added functions from hodl

--- shay-lib.js
+++ shay-lib.js
@@ -55,3 +55,173 @@ function hideSrc(){
         }
     };
 }
+
+
+function randomQuery(floor, ceil, string = "false"){
+    let num = Math.floor(Math.random() * (ceil - floor + 1) + floor);
+
+    if(string == true){
+
+        console.log("String mode set to true")
+        return num.toString();
+    }
+
+    return num;
+    
+} // end randomQuery
+
+function coinFlip(){
+    return randomQuery(0, 1);
+} // end coinFlip
+
+// Run a function repeatedly 'n' number of times [ Doesn't work if we need to pass arguments to it ]
+function repeatFunction(func, n, args = {}){
+    for(let i = 0; i < n; i++){
+        if (args){
+            func(args);
+        } else{
+            func();
+        }
+    } // end loop
+} // end function
+
+
+function twoNums(){
+    let join = '';
+
+    let dec1 = randomQuery(0,9).toString();
+    let dec2 = randomQuery(0,9).toString();
+
+    join += dec1 += dec2;
+
+    return join; // It's a string at this point
+
+}
+
+function setFlashMessage(type, message){
+    switch(type){
+        case 'success':
+            alertBox.classList.add("alert-success");
+            alertBox.classList.remove("hide");
+            alertBox.textContent = message;
+            resetFlashMessage("alert-success");
+            break;
+
+        case 'error':
+            alertBox.classList.add("alert-danger");
+            alertBox.classList.remove("hide");
+            alertBox.textContent = message;
+            resetFlashMessage("alert-danger");
+        break;
+    }
+
+} // setFlashMessage
+
+function resetFlashMessage(removeClass){
+    
+    setTimeout(function(){
+        alertBox.classList.remove(removeClass);
+        alertBox.classList.add("hide");
+        alertBox.textContent = '';
+    }, 1000)
+
+} // resetFlashMessage
+
+
+// Takes a css class, applies it to all elements and removes it from all exceptions (if direction is false, it works in reverse)
+function bulkClassToggler(cssClass = '', elements = [], exceptions = [], direction = true){
+
+    // Add classes before taking anyway (needs to be in this order in case of a duplicated entry in an)
+    elements.forEach((el)=>{
+        
+        switch(direction){
+            case true: // Default
+                el.classList.add(cssClass);
+                break;
+
+            case false: // Go in reverse
+                el.classList.remove(cssClass);
+                break;
+
+
+        }// end switch
+        
+        console.log("An element:")
+        console.log(el)
+        
+    }); // end foreach
+    
+    // Remove class from exceptions
+    exceptions.forEach((x)=>{
+
+        switch(direction){
+            case true: // Default
+                x.classList.remove(cssClass);
+                break;
+
+            case false: // Go in reverse
+                x.classList.add(cssClass);
+                break;
+
+
+        }// end switch
+        
+        
+        console.log("An exception:")
+        console.log(x);
+        
+    }); // end foreach
+                
+            
+} // end bulkClassToggler
+
+// Creates as many html elements as needed according to args
+function htmlFactory(appendTarget, elementToCreate, qty = 1, cssClasses = [], content){
+
+    for(var i = 0; i < qty; i++){
+
+        // Loop through and create as many elements with as many classes as specified in the arguments
+        var htmlElement = document.createElement(elementToCreate);
+
+        if(content){
+            var contentNode = document.createTextNode(content);
+            htmlElement.appendChild(contentNode);
+        }
+
+        // Add classes to element
+        cssClasses.forEach( (cssClass) => {
+            htmlElement.classList.add(cssClass);
+        }); 
+
+        // Append new element to all of them
+        appendTarget.appendChild(htmlElement);
+        
+
+    } // end construction loop
+
+} // end htmlFactory
+
+function htmlBuilder( buildThis ){
+    // Deconstruct the object and build everything into arrays
+    var elements = Object.keys(buildThis);
+    var values = Object.values(buildThis);
+    
+    // Loop through the details and build them accordingly
+    for(var i = 0; i < elements.length; i++){
+        // Grab the attributes
+        var qty = values[i].qty;
+        var cssClass = values[i].class;
+        var content = values[i].content;
+        var appendTarget = values[i].appendTarget;
+
+        // Default values
+        if (qty == undefined){ qty = 1; }
+        if (cssClass == undefined){ cssClass = []; }
+        if (content == undefined){ content = []; }
+        if (appendTarget == undefined){ appendTarget = body; } // Should appendTarget have a setting in consts.js?
+
+        // Now build everything
+        
+    } // end loop
+
+} // end htmlBuilder
