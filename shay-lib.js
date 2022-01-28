0a1ebb9b44d69637cc8a9a3f582aca64dea6b8d8
p3z linux pc <chenille.pottle@yahoo.co.uk>
2022-01-28 19:48:37 +0000

    Added functions to verify uniqueness of array value

--- shay-lib.js
+++ shay-lib.js
@@ -252,3 +252,58 @@ function isUniqueStr(str1, str2){
     }
 
 }
+
+
+// Check a single string against an entire array of strings
+function is_fresh_input(code, arr){
+    
+    var code_in_array = arr.includes(code);
+
+    if(code_in_array){
+        return false;
+    } else{
+        return true;
+    }
+
+}
+
+// Parse through an array of strings and remove duplicates
+// Returns an object containing the number of duplicates removed and the resulting array
+function remove_duplicates(catalog){
+
+    var orig_length = catalog.length;
+
+    //(if needed)
+    // From here: // https://stackoverflow.com/questions/49215358/checking-for-duplicate-strings-in-javascript-array
+    let findDuplicates = arr => arr.filter((item, index) => arr.indexOf(item) != index);
+    
+    // An array of duplicated keys 
+     var duplicates = [...new Set(findDuplicates(catalog))]
+
+
+    // Loop over the catalog, and this time remove all duplicates from it
+    var unique_vals = [...new Set(catalog)];
+    var unique_length = unique_vals.length;  
+
+    return {
+        num_keys_removed: `${duplicates.length}`,
+        duplicates: duplicates,        
+        catalog: unique_vals
+    }
+    
+}
+
+
+
+// Contains duplicates
+let catalog_one = [ "WT7L-CQW0-KCV4-0REK", "0N2E-Y981-S9B5-5STT", "WT7L-CQW0-KCV4-0REK", "TH80-TQ47-S1GO-UTQ6", "F8EM-LG40-2SK1-7N4P", "04ZI-91J5-69Y5-92N2", "WT7L-CQW0-KCV4-0REK", "0N2E-Y981-S9B5-5STT"];
+
+// No duplicates
+let catalog_two = [ "WT7L-CQW0-KCV4-0REK", "0N2E-Y981-S9B5-5STT", "TH80-TQ47-S1GO-UTQ6", "F8EM-LG40-2SK1-7N4P", "04ZI-91J5-69Y5-92N2", "7H3G-78M8-NY7R-8U74", "SPVL-CPXO-0445-869H"];
+
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// console.log(remove_duplicates(catalog_one));
+// console.log(remove_duplicates(catalog_two));
+
+console.log(is_fresh_input("04ZI-91J5-69Y5-92N2", catalog_two))
