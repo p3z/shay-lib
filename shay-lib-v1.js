0c8acba168f07e592f48e828874748f6e845c835
Shay <chenille.pottle@yahoo.co.uk>
2022-09-18 18:28:59 +0100

    Added get_keycode and rand_color picker

--- shay-lib-v1.js
+++ shay-lib-v1.js
@@ -322,4 +322,76 @@ function is_canvas_blank(canvas){
     return !canvas.getContext('2d')
       .getImageData(0, 0, canvas.width, canvas.height)
       .data.some(channel => channel !== 0)
-  }
+}
+
+function get_keycode(key){
+     
+    let read_key = key.toUpperCase() ?? key;
+    
+    const key_map = {
+        A:65,
+        B:66,
+        C:67,
+        D:68,
+        E:69,
+        F:70,
+        G:71,
+        H:72,
+        I:73,
+        J:74,
+        K:75,
+        L:76,
+        M:77,
+        N:78,
+        O:79,
+        P:80,
+        Q:81,
+        R:82,
+        S:83,
+        T:84,
+        U:85,
+        V:86,
+        W:87,
+        X:88,
+        Y:89,
+        Z:90,
+        0:48,
+        1:49,
+        2:50,
+        3:51,
+        4:52,
+        5:53,
+        6:54,
+        7:55,
+        8:56,
+        9:57,
+        BACKSPACE:8,
+        TAB:9,
+        ENTER:13,
+        SHIFT:16,
+        CTRL:17,
+        ALT:18,
+        LEFT: 37,
+        UP: 38,
+        RIGHT: 39,
+        DOWN: 40         
+    }
+
+    return key_map[read_key] ?? "Couldnt find key";
+    
+}
+
+function random_color(alpha = false){
+    if(alpha){
+        return'rgba(' + 
+            Math.floor(Math.random() * 256) + ',' + 
+            Math.floor(Math.random() * 256) + ',' + 
+            Math.floor(Math.random() * 256) + ',' + 
+            Math.floor(Math.random() ) + ')'; // Alpha channel
+    } else {
+        return'rgb(' + 
+            Math.floor(Math.random() * 256) + ',' + 
+            Math.floor(Math.random() * 256) + ',' + 
+            Math.floor(Math.random() * 256) + ')';
+    }
+}
